6afb61e9afc4eef6512ccba0cfac4d9b5e984ffb
Rikesh Subedi <rsubedi3@gatech.edu>
2017-04-01 02:27:13 -0400

    Modify UI left drawer

--- app/src/main/java/me/ryanpetschek/gatekeeper/MainActivity.java
+++ app/src/main/java/me/ryanpetschek/gatekeeper/MainActivity.java
@@ -80,17 +80,18 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_id) {
+            // Handle the ID action
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_nearby) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_network) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_account) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_perm_given) {
+
+        } else if (id == R.id.nav_perm_received) {
 
         }
 
