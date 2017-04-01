88a86273e61d187b2153ecee4e62d01f4d350ae7
bentemplin <ben.d.templin@gmail.com>
2017-04-01 16:37:26 -0400

    Optimized import statements in SignInActivity

--- app/src/main/java/me/ryanpetschek/gatekeeper/SignInActivity.java
+++ app/src/main/java/me/ryanpetschek/gatekeeper/SignInActivity.java
@@ -2,21 +2,17 @@ package me.ryanpetschek.gatekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.spec.ECParameterSpec;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
-
-import org.bouncycastle.jce.ECNamedCurveTable;
-import org.bouncycastle.jce.spec.ECParameterSpec;
 
 public class SignInActivity extends AppCompatActivity {
 
