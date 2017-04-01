75c5c9658ec7e3c68b342157415745f7c0c83452
Ryan Petschek <petschekr@gmail.com>
2017-04-01 16:59:18 -0400

    Validate inputs in sign up activity

--- app/src/main/java/me/ryanpetschek/gatekeeper/SignInActivity.java
+++ app/src/main/java/me/ryanpetschek/gatekeeper/SignInActivity.java
@@ -2,10 +2,14 @@ package me.ryanpetschek.gatekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.webkit.URLUtil;
 
 import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.spec.ECParameterSpec;
@@ -25,8 +29,29 @@ public class SignInActivity extends AppCompatActivity {
         setTitle("Sign up");
 
         final Button button = (Button) findViewById(R.id.signup);
+        final EditText nameField = (EditText) findViewById(R.id.name);
+        final EditText pictureUrlField = (EditText) findViewById(R.id.photoUrl);
+        final AlertDialog alertDialog  = new AlertDialog.Builder(this).create();
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                // Validate inputs
+                String name = nameField.getText().toString().trim();
+                String pictureUrl = pictureUrlField.getText().toString().trim();
+                if (name.length() == 0 || pictureUrl.length() == 0) {
+                    alertDialog.setTitle("Error");
+                    alertDialog.setMessage("Name or photo URL cannot be left blank");
+                    alertDialog.show();
+                    return;
+                }
+                if (!URLUtil.isValidUrl(pictureUrl)) {
+                    alertDialog.setTitle("Error");
+                    alertDialog.setMessage("Invalid photo URL");
+                    alertDialog.show();
+                    return;
+                }
+
+                KeyPair keyPair = generateKeys();
+
                 Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                 startActivity(intent);
             }
@@ -41,14 +66,8 @@ public class SignInActivity extends AppCompatActivity {
             KeyPair pair = g.generateKeyPair();
             return pair;
         }
-        catch (java.security.NoSuchAlgorithmException err) {
-
-        }
-        catch (java.security.NoSuchProviderException err) {
-
-        }
-        catch (java.security.InvalidAlgorithmParameterException err) {
-
+        catch (java.security.NoSuchAlgorithmException | java.security.NoSuchProviderException | java.security.InvalidAlgorithmParameterException err) {
+            Log.e("KeyGen", err.getMessage());
         }
         finally {
             return null;
