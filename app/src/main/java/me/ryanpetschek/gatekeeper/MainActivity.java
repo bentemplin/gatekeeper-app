b37af15f4fcbcdc73d428a27111b1a3101962726
bentemplin <ben.d.templin@gmail.com>
2017-04-01 22:21:45 -0400

    Disable back button

--- app/src/main/java/me/ryanpetschek/gatekeeper/MainActivity.java
+++ app/src/main/java/me/ryanpetschek/gatekeeper/MainActivity.java
@@ -70,12 +70,12 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
     }
 
     @Override
