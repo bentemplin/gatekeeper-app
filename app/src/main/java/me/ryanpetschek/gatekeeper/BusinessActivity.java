a345d35f539fa52bc63b712683b0ad6ab5c2edcb
Carl Schriever <Carl Schriever>
2017-04-02 05:53:52 -0400

    Business UI fixed

--- app/src/main/java/me/ryanpetschek/gatekeeper/BusinessActivity.java
+++ app/src/main/java/me/ryanpetschek/gatekeeper/BusinessActivity.java
@@ -7,36 +7,32 @@ import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-imp
-
 public class BusinessActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setTitle("Building");
+
         Building source = Nearby.currentBuilding;
         Bitmap image = source.getImage();
         String name = source.getName();
         String address = source.getAddress();
         String desc = source.getDescription();
 
-        v = inflater.inflate(R.layout.fragment_id, container, false);
-        ImageView imageView = (ImageView) v.findViewById(R.id.buildingImage);
-        TextView nameText = (TextView) v.findViewById(R.id.txtNameLabel);
-        TextView addrText = (TextView) v.findViewById(R.id.txtAddressLabel);
-        TextView descText = (TextView) v.findViewById(R.id.txtDescLabel);
+        ImageView imageView = (ImageView) findViewById(R.id.buildingImage);
+        TextView nameText = (TextView) findViewById(R.id.txtName);
+        TextView addrText = (TextView) findViewById(R.id.txtAddress);
+        TextView descText = (TextView) findViewById(R.id.txtDesc);
 
         imageView.setImageBitmap(image);
         nameText.setText(name);
         addrText.setText(address);
         descText.setText(desc);
-
-
-        setContentView(R.layout.activity_business);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        setTitle("Building");
     }
 
 }
