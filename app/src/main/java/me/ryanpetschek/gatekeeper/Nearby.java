418c8adf2c60116dc40db15034032de63b5a7258
bentemplin <ben.d.templin@gmail.com>
2017-04-02 01:01:59 -0400

    Integrate map user location and building markers

--- app/src/main/java/me/ryanpetschek/gatekeeper/Nearby.java
+++ app/src/main/java/me/ryanpetschek/gatekeeper/Nearby.java
@@ -12,11 +12,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.util.Log;
 
 
 public class Nearby extends FragmentActivity implements OnMapReadyCallback,
@@ -34,7 +36,6 @@ public class Nearby extends FragmentActivity implements OnMapReadyCallback,
         setTitle("Nearby Buildings");
 
         mLocMan = (LocationManager) getSystemService(LOCATION_SERVICE);
-
         setContentView(R.layout.activity_nearby);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -54,11 +55,23 @@ public class Nearby extends FragmentActivity implements OnMapReadyCallback,
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        //getLocationPermission();
-        mMap = googleMap;
+        getLocationPermission();
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                 REQUEST_CODE_ASK_PERMISSIONS);
+        mMap = googleMap;
+        LatLng sydney = new LatLng(33.7563, -84.3893);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Alderhold"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng hopkins = new LatLng(33.7741, -84.3913);
+        mMap.addMarker(new MarkerOptions().position(hopkins).title("Hopkins Hall"));
+        LatLng anthem = new LatLng(33.7772, -84.3891);
+        mMap.addMarker(new MarkerOptions().position(anthem).title("Anthem Innovation Studio"));
+        LatLngBounds bounds = new LatLngBounds(new LatLng(33.75, -84.44), new LatLng(33.8, -84.34));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 0));
+
+//        mLoc = mLocMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//        mMap.setMyLocationEnabled(true);
 
 //        LatLng me = new LatLng(mLoc.getLatitude(), mLoc.getLongitude());
 //        mMap.addMarker(new MarkerOptions().position(me).title("Me"));
@@ -66,16 +79,16 @@ public class Nearby extends FragmentActivity implements OnMapReadyCallback,
     }
 
 
-//    public void getLocationPermission() {
-//        if (Build.VERSION.SDK_INT >= 23) {
-//        int hasLocationPermission = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
-//        if (hasLocationPermission != PackageManager.PERMISSION_GRANTED) {
-//            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
-//                        REQUEST_CODE_ASK_PERMISSIONS);
-//            }
-//            return;
-//        }
-//    }
+    public void getLocationPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+        int hasLocationPermission = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+        if (hasLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+            }
+            return;
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -98,28 +111,24 @@ public class Nearby extends FragmentActivity implements OnMapReadyCallback,
                         //                                          int[] grantResults)
                         // to handle the case where the user grants the permission. See the documentation
                         // for ActivityCompat#requestPermissions for more details.
-                        mLoc = mLocMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        mMap.setMyLocationEnabled(true);
-                        LatLng me = new LatLng(mLoc.getLatitude(), mLoc.getLongitude());
-                        mMap.addMarker(new MarkerOptions().position(me).title("Me"));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(me));
+                        Log.d("LAT", String.valueOf(mLoc.getLatitude()));
+                        Log.d("LONG", String.valueOf(mLoc.getLongitude()));
+
                         return;
+                        //}
+                    } else {
+
+                        // permission denied, boo! Disable the
+                        // functionality that depends on this permission.
                     }
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                    mMap.setMyLocationEnabled(true);
+                    return;
                 }
-                return;
-            } default: {
-                System.out.println(requestCode);
+
+                // other 'case' lines to check for other
+                // permissions this app might request
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
-
+        }}
     @Override
     public void onFragmentInteraction(Uri uri) {
         //empty b/c not implemented
